7b201e32158cecf48abe5a47783c2b1c8c63c13f
kmbui <khanhmb815@gmail.com>
2021-10-20 18:22:53 -0700

    cloning

---
+++ _site/precache-manifest.38db937d6862e0eeb63c46957ecad7e1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8496743dbadbde6ddc511673b3ffa87c",
+    "url": "/protein-visualizer/index.html"
+  },
+  {
+    "revision": "d468b181a2c8ba864079",
+    "url": "/protein-visualizer/static/css/main.85a7a5b1.chunk.css"
+  },
+  {
+    "revision": "4d7ebaca67b00bdcecbc",
+    "url": "/protein-visualizer/static/js/2.347b1bb1.chunk.js"
+  },
+  {
+    "revision": "6472a7d1d95598048b2f7b02095da167",
+    "url": "/protein-visualizer/static/js/2.347b1bb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d468b181a2c8ba864079",
+    "url": "/protein-visualizer/static/js/main.11a86d0f.chunk.js"
+  },
+  {
+    "revision": "e58b14c0a1e067f8db8f",
+    "url": "/protein-visualizer/static/js/runtime-main.570c692a.js"
+  },
+  {
+    "revision": "80b7f8bb6ee9684a7f17e7852e185c9f",
+    "url": "/protein-visualizer/static/media/proteins.80b7f8bb.csv"
+  },
+  {
+    "revision": "730a03c0c12c41ff9353284351dda02f",
+    "url": "/protein-visualizer/static/media/sunlab.730a03c0.png"
+  }
+]);
